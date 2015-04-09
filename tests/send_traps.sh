0565577b4820870556558923d8e0f86f9f5e9219
Matt Hudgins <mhudgins88@gmail.com>
2015-04-08 18:39:28 -0700

    User configurable community string
    Users can specify a community string in trapperkeeper.yaml. If
    trapperkeeper recieves a trap whose community string doesn't match the
    configured community string, trapperkeepr will discard the trap and log
    a message:
    
    WARNING:root:Trapperkeeper community is: public, received trap with
    community: private..discarding

--- tests/send_traps.sh
+++ tests/send_traps.sh
@@ -4,7 +4,9 @@ BASE_DIR=$( dirname $0 )
 
 DEST="localhost"
 COMMON_OPTS="-Ln -mTRAPPERKEEPER-MIB -M+"${BASE_DIR}/mibs" -c public"
+PRIV_COMMON_OPTS="-Ln -mTRAPPERKEEPER-MIB -M+"${BASE_DIR}/mibs" -c private"
 SYSLOC_VARBIND="SNMPv2-MIB::sysLocation.0 s TrapperKeeper-Test"
 
 snmptrap -v 1 $COMMON_OPTS $DEST TRAPPERKEEPER-MIB::testtraps "" 6 1 "" $SYSLOC_VARBIND
 snmptrap -v 2c $COMMON_OPTS $DEST "" TRAPPERKEEPER-MIB::testV2Trap $SYSLOC_VARBIND
+snmptrap -v 1 $PRIV_COMMON_OPTS $DEST TRAPPERKEEPER-MIB::testtraps "" 6 1 "" $SYSLOC_VARBIND
